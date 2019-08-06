a7733fce300f9532c9a96e7194b4374b9c64797b
anton <ar.smirnov@jet.msk.su>
2019-08-06 17:07:26 +0300

    updated print in task1

--- hometask1/src/main/java/com/javalesson/chapter1/task2/GradeCalculatorTask.java
+++ hometask1/src/main/java/com/javalesson/chapter1/task2/GradeCalculatorTask.java
@@ -32,7 +32,7 @@ package com.javalesson.chapter1.task2;
 public class GradeCalculatorTask {
 
     public static void main(String[] args) {
-        System.out.println("for a pr!");
+        System.out.println("for a pr from intellij idea!");
 
     }
 }
